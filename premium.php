1afe1cf83a264eee03519ccc031e0a61eb5e6416
xbasetools <127048636+xbasetools@users.noreply.github.com>
2023-03-07 10:05:00 -0800

    Update premium.php

--- premium.php
+++ premium.php
@@ -597,7 +597,32 @@ let table = new DataTable('#myTable', {
 </div>
 
 <script>
-$('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#account_country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#account_sitename").text().toLowerCase() );var ck3 = $.trim( $(this).find("#account_seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="account_country"]').val().toLowerCase() );var val2 = $.trim( $('input[name="account_sitename"]').val().toLowerCase() );var val3 = $.trim( $('select[name="account_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != '' )){ $(this).hide();  }else{ $(this).show(); } });$('#filterbutton').prop('disabled', true);});$('.filterselect').change(function () {$('#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
+$('#filterbutton').click(function () 
+{$("#table tbody tr").each(function() {var ck1 = $.trim( 
+												                                    $(this).find("#country").text().toLowerCase() );var ck2 = 
+																    $.trim( 
+																    $(this).find("#sitename").text().toLowerCase() );var ck3 =
+																    $.trim( 
+																    $(this).find("#idseller").text().toLowerCase() ); var val1 = 
+																    $.trim( 
+																    $('select[name="country"]').val().toLowerCase() );var val2 = 
+																    $.trim(
+																    $('input[name="sitename"]').val().toLowerCase() );var val3 =        
+																    $.trim( 
+																    $('select[name="seller"]').val().toLowerCase() );
+																     if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != '' )){ 
+																    
+																    $(this).hide();  
+																    }else{ 
+																    
+																    $(this).show(); } });
+																    $('#filterbutton').prop('disabled', true
+																    );});
+																    $('.filterselect').change(function () 
+																    {
+																    $('#filterbutton').prop('disabled', false);});
+																    $('.filterinput').keyup(function () {
+																    $('#filterbutton').prop('disabled', false);});
 function buythistool(id){
   bootbox.confirm("Are you sure?", function(result) {
         if(result ==true){
