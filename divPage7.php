5e13ea0ab12535dc9579473c5e534a0124871c8d
xbasetools <127048636+xbasetools@users.noreply.github.com>
2023-03-07 23:04:57 +0100

    Update divPage7.php

--- divPage7.php
+++ divPage7.php
@@ -10,65 +10,50 @@ if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
 }
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>
-<div class="alert alert-info text-left" role="alert" style="margin: 15px;">
-      <ul>
-        <li>For Any problem for account after buy just open report and seller will fix it or replace.</li>
-        <li>There is <b> 133 </b> Accounts Available.</li>
-      </ul>
-    </div>
-    <input type=hidden id="cat" name="cat" value="7" />
-    <div class="row m-3 pt-1" style="color: var(--font-color);">
-      <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
-        <label for="infos" style="margin-bottom: 10px; margin-top: 5px">Website Name :</label>
-        <select name="sitename" id="sitename" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
-          <option value="">All</option>
-          <?php
-          $query = mysqli_query($dbcon, "SELECT DISTINCT(`sitename`) FROM `accounts` WHERE `sold` = '0' ORDER BY sitename. ASC");
-          while($row = mysqli_fetch_assoc($query)){
-          echo '<option value="'.$row['sitename'].'">'.$row['sitename'].'</option>';
-          }
-          ?>
-        </select>
-      </div>
-      <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
-        <label for="infos" style="margin-bottom: 10px; margin-top: 5px">Details:</label>
-        <input type="search" class="form-control" id="infos" style="color: var(--font-color); background-color: var(--color-card);">
-      </div>
-      <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
-        <label for="Country" style="margin-bottom: 10px; margin-top: 5px">Country :</label>
-        <select name="country" id="country" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
-          <?php
-          $query = mysqli_query($dbcon, "SELECT DISTINCT(`country`) FROM `accounts` WHERE `sold` = '0' ORDER BY country ASC");
-          while($row = mysqli_fetch_assoc($query)){
-          echo '<option value="'.$row['country'].'">'.$row['country'].'</option>';
-          }
-          ?>
-          <option value="">All Countries</option>
-        </select>
-        
-        
-      </div>
-      <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
-        <label for="seller" style="margin-bottom: 10px; margin-top: 5px">Seller :</label>
-        <select name="seller" id="seller" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
-      
-                            <option value="">All Seller</option>
-																     <?php
-          $query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `accounts` WHERE `sold` = '0' ORDER BY resseller ASC");
-          while($row = mysqli_fetch_assoc($query)){
-          $qer = mysqli_query($dbcon, "SELECT DISTINCT(`id`) FROM resseller WHERE username='".$row['resseller']."' ORDER BY id ASC")or die(mysql_error());
-          while($rpw = mysqli_fetch_assoc($qer))
-          $SellerNick = "seller".$rpw["id"]."";
-          echo '<option value="'.$SellerNick.'">'.$SellerNick.'</option>';
-          }
-          ?>
-          
-        </select>
-      </div>
-    </div>
 
-
+<ul class="nav nav-tabs">
+  <li class="active"><a href="#filter" data-toggle="tab">Filter</a></li>
+</ul>
+<div id="myTabContent" class="tab-content" >
+  <div class="tab-pane active in" id="filter"><table class="table"><thead><tr><th>Country</th>
+<th>Site Name</th>
+<th>Seller</th>
+<th></th></tr></thead><tbody><tr><td><select class='filterselect form-control input-sm' name="account_country"><option value="">ALL</option>
 <?php
+$query = mysqli_query($dbcon, "SELECT DISTINCT(`country`) FROM `accounts` WHERE `sold` = '0' ORDER BY country ASC");
+	while($row = mysqli_fetch_assoc($query)){
+	echo '<option value="'.$row['country'].'">'.$row['country'].'</option>';
+	}
+?>
+</select></td><td><input class='filterinput form-control input-sm' name="account_sitename" size='3'></td><td><select class='filterselect form-control input-sm' name="account_seller"><option value="">ALL</option>
+<?php
+$query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `accounts` WHERE `sold` = '0' ORDER BY resseller ASC");
+	while($row = mysqli_fetch_assoc($query)){
+		 $qer = mysqli_query($dbcon, "SELECT DISTINCT(`id`) FROM resseller WHERE username='".$row['resseller']."' ORDER BY id ASC")or die(mysql_error());
+		   while($rpw = mysqli_fetch_assoc($qer))
+			 $SellerNick = "seller".$rpw["id"]."";
+	echo '<option value="'.$SellerNick.'">'.$SellerNick.'</option>';
+	}
+?>
+</select></td><td><button id='filterbutton'class="btn btn-primary btn-sm" disabled>Filter <span class="glyphicon glyphicon-filter"></span></button></td></tr></tbody></table></div>
+</div>
+
+
+<table width="100%"  class="table table-striped table-bordered table-condensed sticky-header" id="table">
+<thead>
+    <tr>
+      <th scope="col" >Country</th>
+      <th scope="col">Site Name</th>
+      <th scope="col">Available Information</th>
+      <th scope="col">Seller</th>
+      <th scope="col">Price</th>
+      <th scope="col">Added on </th>
+      <th scope="col">Buy</th>
+    </tr>
+</thead>
+  <tbody>
+
+ <?php
 include("cr.php");
 $q = mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0' ORDER BY RAND()")or die(mysqli_error());
  while($row = mysqli_fetch_assoc($q)){
@@ -96,3 +81,38 @@ $q = mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0' ORDER BY RAND()
  }
 
  ?>
+<script type="text/javascript">
+$('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#account_country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#account_sitename").text().toLowerCase() );var ck3 = $.trim( $(this).find("#account_seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="account_country"]').val().toLowerCase() );var val2 = $.trim( $('input[name="account_sitename"]').val().toLowerCase() );var val3 = $.trim( $('select[name="account_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != '' )){ $(this).hide();  }else{ $(this).show(); } });$('#filterbutton').prop('disabled', true);});$('.filterselect').change(function () {$('#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
+function buythistool(id){
+  bootbox.confirm("Are you sure?", function(result) {
+        if(result ==true){
+      $.ajax({
+     method:"GET",
+     url:"buytool.php?id="+id+"&t=accounts",
+     dataType:"text",
+     success:function(data){
+         if(data.match(/<button/)){
+		 $("#account"+id).html(data).show();
+         }else{
+            bootbox.alert('<center><img src="files/img/balance.png"><h2><b>No enough balance !</b></h2><h4>Please refill your balance <a class="btn btn-primary btn-xs"  href="addBalance.html" onclick="window.open(this.href);return false;" >Add Balance <span class="glyphicon glyphicon-plus"></span></a></h4></center>')
+         }
+     },
+   });
+       ;}
+  });
+}
+
+function openitem(order){
+  $("#myModalLabel").text('Order #'+order);
+  $('#myModal').modal('show');
+  $.ajax({
+    type:       'GET',
+    url:        'showOrder'+order+'.html',
+    success:    function(data)
+    {
+        $("#modelbody").html(data).show();
+    }});
+
+}
+
+</script>
