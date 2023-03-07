3c81969f1cdce98c9c68c1c24356fcece3f31eb4
xbasetools <127048636+xbasetools@users.noreply.github.com>
2023-03-08 00:19:41 +0100

    Update premium.php

--- premium.php
+++ premium.php
@@ -373,12 +373,10 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 
 
 
-                            </tr>
+              
                         </thead>
                     </table>
-                </div>
-            </div>
-        </div>
+              </div>
     </div>
     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
         <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-notify modal-success" role="document">
@@ -416,11 +414,8 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
                     <a type="button" class="btn btn-info" data-dismiss="modal">No</a>
                 </div>
             </div>
- 
         </div>
     </div>
- 
- 
     <div class="modal fade top" id="modalCoupon" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
         <div class="modal-dialog modal-frame modal-top modal-notify modal-danger" role="document">
  
@@ -440,5 +435,95 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
  
         </div>
     </div>
- 
-    
+<script>
+             function ajaxinfo() {
+                $.ajax({
+                    type: ‘GET’,
+                    url: ‘ajaxinfo.html’,
+                    timeout: 10000,
+
+                    success: function(data) {
+                        if (data != ‘01’) {
+                            var data = JSON.parse(data);
+                            for (var prop in data) {
+                                $(“#” + prop).html(data[prop]).show();
+                            }
+                        } else {
+                            window.location = “logout.html”;
+                        }
+                    }
+                });
+
+            }
+            setInterval(function() {
+                ajaxinfo()
+            }, 3000);
+
+            ajaxinfo();
+
+$(document).keydown(function(event){
+    if(event.which==“17”)
+        cntrlIsPressed = true;
+});
+
+$(document).keyup(function(){
+    cntrlIsPressed = false;
+});
+
+var cntrlIsPressed = false;
+
+
+function pageDiv(n,t,u,x){
+  if(cntrlIsPressed){
+    window.open(u, ‘_blank’);
+    return false;
+  }
+        var obj = { Title: t, Url: u };
+        if ( (“/“+obj.Url) != location.pathname) {
+        	if (x != 1) {history.pushState(obj, obj.Title, obj.Url);}
+        	else{history.replaceState(obj, obj.Title, obj.Url);}
+
+    	}
+      document.title = obj.Title;
+    $(“#mainDiv”).html(‘<div id=“mydiv”><img src=“files/img/load2.gif” class=“ajax-loader”></div>’).show();
+    $.ajax({
+    type:       ‘GET’,
+      url:        ‘divPage’+n+’.html’,
+       success:    function(data)
+       {
+   
+   
+   
+   
+      $(“#mainDiv”).html(data).show();
+        newTableObject = document.getElementById(‘table’);
+        sorttable.makeSortable(newTableObject);
+        $(“.sticky-header”).floatThead({top:60});
+        if(x==0){ajaxinfo();}
+         }});
+         if (typeof stopCheckBTC === ‘function’) { 
+          var a = stopCheckBTC();
+     }
+
+}
+
+$(window).on(“popstate”, function(e) {
+        location.replace(document.location);
+
+});
+
+
+function setTooltip(btn, message) {
+  console.log(“hide-1”);
+  $(btn).tooltip(‘hide’)
+    .attr(‘data-original-title’, message)
+    .tooltip(‘show’);
+     console.log(“show”);
+}
+
+function hideTooltip(btn) {
+  setTimeout(function() {$(btn).tooltip(‘hide’); console.log(“hide-2”);}, 1000);
+}
+</script>
+    </body>
+    </head>
