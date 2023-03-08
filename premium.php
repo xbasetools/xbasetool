f2356f674cdf7cbf032f230b4863038784d3c7b6
xbasetools <127048636+xbasetools@users.noreply.github.com>
2023-03-08 01:32:59 -0800

    Update premium.php

--- premium.php
+++ premium.php
@@ -479,19 +479,9 @@ a.closearb {
 <thead>
 <tr>
 <th data-priority="1"></th>
-<th class="all">ID</th>
-<th data-priority="3">Website Name</th>
-<th data-priority="4">Country</th>
-<th data-priority="7">Details</th>
-<th data-priority="8">Price</th>
-<th data-priority="9">Seller</th>
-<th data-priority="10">Source</th>
-<th class="all">Proof</th>
-<th data-priority="11">Date Created</th>
-<th class="all">Buy</th>
-</tr>
-</thead>
-</table>
+ 
+ 
+ 
 </div>
 </div>
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
