030b9c8471ff4e64e35663297ca267a6bd932a5f
xbasetools <127048636+xbasetools@users.noreply.github.com>
2023-03-08 08:15:21 -0800

    Update premium.php

--- premium.php
+++ premium.php
@@ -457,9 +457,9 @@ Send
     <!-- /.navbar-collapse -->
   </div>
   <!-- /.container-fluid -->
-</nav>
+</nav>    <div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
  <div id="mainDiv">
-    <div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
+
     
     
     
