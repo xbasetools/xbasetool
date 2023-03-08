5fa8996725cb20c7138365225733f85eefa0f9ac
xbasetools <127048636+xbasetools@users.noreply.github.com>
2023-03-08 02:12:45 -0800

    Update divPage7.php

--- divPage7.php
+++ divPage7.php
@@ -1,8 +1,8 @@
- <?php
+<?php
 ob_start();
 session_start();
 date_default_timezone_set(‘UTC’);
-include “includes/config.php”;
+include “../includes/config.php”;
 
 if (!isset($_SESSION[‘sname’]) and !isset($_SESSION[‘spass’])) {
     header(“location: ../“);
@@ -35,16 +35,23 @@ $query = mysqli_query($dbcon, “SELECT DISTINCT(`resseller`) FROM `accounts` WH
 	echo ‘<option value=“’.$SellerNick.’”>’.$SellerNick.’</option>’;
 	}
 ?>
-</select></td><td><button id=‘filterbutton’class=“btn btn-primary btn-sm” disabled>Filter <span class=“glyphicon glyphicon-filter”></span></button></td></tr></tbody></table></div>
+</select>
+</div>
+</td>
+
+<td><button id=‘filterbutton’class=“btn btn-primary btn-sm” disabled>search <span class=“fa fa-filter”></span></button></td></tr>
+</tbody>
+</table>
+</div>
 </div>
 
 
-<table width=“100%”  class=“table table-striped table-bordered table-condensed sticky-header” id=“table”>
+<table width=“100%”  class=“table table-striped table-bordered table-condensed sticky-header” id=“account_data”>
 <thead>
     <tr>
       <th scope=“col” >Country</th>
       <th scope=“col”>Site Name</th>
-      <th scope=“col”>Available Information</th>
+      <th scope=“col”>Website Name</th>
       <th scope=“col”>Seller</th>
       <th scope=“col”>Price</th>
       <th scope=“col”>Added on </th>
@@ -82,7 +89,9 @@ $q = mysqli_query($dbcon, “SELECT * FROM accounts WHERE sold=‘0’ ORDER BY 
 
  ?>
 <script type=“text/javascript”>
-$(‘#filterbutton’).click(function () {$(“#table tbody tr”).each(function() {var ck1 = $.trim( $(this).find(“#account_country”).text().toLowerCase() );var ck2 = $.trim( $(this).find(“#account_sitename”).text().toLowerCase() );var ck3 = $.trim( $(this).find(“#account_seller”).text().toLowerCase() ); var val1 = $.trim( $(‘select[name=“account_country”]’).val().toLowerCase() );var val2 = $.trim( $(‘input[name=“account_sitename”]’).val().toLowerCase() );var val3 = $.trim( $(‘select[name=“account_seller”]’).val().toLowerCase() ); if((ck1 != val1 && val1 != ‘’ ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != ‘’ )){ $(this).hide();  }else{ $(this).show(); } });$(‘#filterbutton’).prop(‘disabled’, true);});$(‘.filterselect’).change(function () {$(‘#filterbutton’).prop(‘disabled’, false);});$(‘.filterinput’).keyup(function () {$(‘#filterbutton’).prop(‘disabled’, false);});
+$(‘#filterbutton’).click(function () {$(“#table tbody tr”).each(function() {var ck1 = $.trim( $(this).find(“#account_country”).text().toLowerCase() );var ck2 = $.trim( $(this).find(“#account_sitename”).text().toLowerCase() );var ck3 = $.trim( $(this).find(“#account_seller”).text().toLowerCase() ); var val1 = $.trim( $(‘select[name=“account_country”]’).val().toLowerCase() );var val2 = $.trim( $(‘input[name=“account_sitename”]’).val().toLowerCase() );var val3 = $.trim( $(‘select[name=“account_seller”]’).val().toLowerCase() ); if((ck1 != val1 && val1 != ‘’ ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != ‘’ )){ $(this).hide();  }else{ $(this).show(); } });$(‘#filterbutton’).prop(‘disabled’, true);});$(‘.filterselect’).change(function () {$(‘#filterbutton’).prop(‘disabled’, false);});$(‘.search’).keyup(function () {$(‘#filterbutton’).prop(‘disabled’, false);});
+
+
 function buythistool(id){
   bootbox.confirm(“Are you sure?”, function(result) {
         if(result ==true){
