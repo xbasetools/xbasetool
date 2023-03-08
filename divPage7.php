19ac9349d4667549312b6237dd097972e7bc34d9
xbasetools <127048636+xbasetools@users.noreply.github.com>
2023-03-08 03:34:52 -0800

    Update divPage7.php

--- divPage7.php
+++ divPage7.php
@@ -12,33 +12,70 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>
 
 <ul class="nav nav-tabs">
-  <li class="active"><a href="#filter" data-toggle="tab">Filter</a></li>
-</ul>
+      </a>
+    </li>
+</ul> 
 <div id="myTabContent" class="tab-content" >
-  <div class="tab-pane active in" id="filter"><table class="table"><thead><tr><th>Country</th>
-<th>Site Name</th>
-<th>Seller</th>
-<th></th></tr></thead><tbody><tr><td><select class='filterselect form-control input-sm' name="account_country"><option value="">ALL</option>
+<div class="d-flex flex-row-reverse mt-0">
+<div class="p-2">
+<label id="switch" class="switch">
+<input type="checkbox" onchange="toggleTheme()" id="slider">
+<span class="slider round">
+</span>
+</label>
+</div>
+</div>
+
+<div class="alert alert-info text-left" role="alert" style="margin: 15px;">
+<ul><li>For Any problem for account after buy just open report and seller will fix it or replace.</li><li>There is <b> 109 </b> Accounts Available.</li></ul>
+<!---website Infos--->
+</div>
+<input type=hidden id="search" name="search" value="website_name" />
+
+<div class="row m-3 pt-1" style="color: var(--font-color);">
+<div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
+<label for="infos" style="margin-bottom: 10px; margin-top: 5px">Website Info :</label>
+<?php
+$query = mysqli_query($dbcon, "SELECT DISTINCT(`website_name`) FROM `accounts` WHERE `sold` = '0' ORDER BY country ASC");
+	while($row = mysqli_fetch_assoc($query)){
+	echo '<option value="'.$row['website_name'].'">'.$row['info'].'</option>';
+	}
+?>
+<select name="website_name" id="website_name" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
+<option value="">All Websites</option>
+ </select>
+
+<!---Countries search--->
+</div>
+<div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
+<label for="infos" style="margin-bottom: 10px; margin-top: 5px">Details:</label>
+<input type="search" class="form-control" id="infos" style="color: var(--font-color); background-color: var(--color-card);">
+</div>
+<div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
+<label for="Country" style="margin-bottom: 10px; margin-top: 5px">Country :</label>
 <?php
 $query = mysqli_query($dbcon, "SELECT DISTINCT(`country`) FROM `accounts` WHERE `sold` = '0' ORDER BY country ASC");
 	while($row = mysqli_fetch_assoc($query)){
-	echo '<option value="'.$row['country'].'">'.$row['country'].'</option>';
-	}
-?>
+	echo '<option value="'.$row['country'].'">'.$row['country'].'</option>';}?>
+<select name="country" id="country" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
+<option value="">All Countries</option>
+</select>
 
-</select></td><td><input class='filterinput form-control input-sm' name="account_sitename" size='3'></td><td><select class='filterselect form-control input-sm' name="account_seller"><option value="">ALL</option>
+<!---seller--->
+</div>
+<div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
+<label for="seller" style="margin-bottom: 10px; margin-top: 5px">Seller :</label>
 <?php
 $query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `accounts` WHERE `sold` = '0' ORDER BY resseller ASC");
 	while($row = mysqli_fetch_assoc($query)){
 		 $qer = mysqli_query($dbcon, "SELECT DISTINCT(`id`) FROM resseller WHERE username='".$row['resseller']."' ORDER BY id ASC")or die(mysql_error());
 		   while($rpw = mysqli_fetch_assoc($qer))
 			 $SellerNick = "seller".$rpw["id"]."";
-	echo '<option value="'.$SellerNick.'">'.$SellerNick.'</option>';
-	}
-?>
-</select></td><td><button id='filterbutton'class="btn btn-primary btn-sm" disabled>Filter <span class="glyphicon glyphicon-filter"></span></button></td></tr></tbody></table></div>
+	echo '<option value="'.$SellerNick.'">'.$SellerNick.'</option>';}?>
+<select name="seller" id="seller" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
+<option value="">All Sellers</option>
+ </select>
 </div>
-
 
 <table width="100%"  class="table table-striped table-bordered table-condensed sticky-header" id="table">
 <thead>
