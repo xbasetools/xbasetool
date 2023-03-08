131596221d86e59fb379f3d13a6971ad366b6919
xbasetools <127048636+xbasetools@users.noreply.github.com>
2023-03-08 04:59:34 -0800

    Update premium.php

--- premium.php
+++ premium.php
@@ -403,7 +403,7 @@ input:checked + .slider:before {
         $('.dropdown').hover(function() {
             $('.dropdown-toggle', this).trigger('click');
         });
-        pageDiv(7, 'Premium/Dating/Shopping - JeruxShop', 'premium.html', 1);
+        pageDiv(7, 'Premium/Dating/Shopping - ', 'accounts', 1);
         var clipboard = new Clipboard('.copyit');
         clipboard.on('success', function(e) {
             setTooltip(e.trigger, 'Copied!');
