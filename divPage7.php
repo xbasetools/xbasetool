7e86c0dadc14970ca0f0207489ef317545d613ed
xbasetools <127048636+xbasetools@users.noreply.github.com>
2023-03-08 06:57:37 -0800

    Update divPage7.php

--- divPage7.php
+++ divPage7.php
@@ -96,26 +96,26 @@ $query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `accounts` WHER
     </tbody>
   </table>
 </div>
+
  <?php
 include("cr.php");
 $q = mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0' ORDER BY RAND()")or die(mysqli_error());
  while($row = mysqli_fetch_assoc($q)){
 	 
 	 	 $countryfullname = $row['country'];
-	     $code = array_search("$countryfullname", $countrycodes);
-	     $countrycode = strtolower($code);
+	  $code = array_search("$countryfullname", $countrycodes);
+	 $countrycode = strtolower($code);
 	    $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
 		   while($rpw = mysqli_fetch_assoc($qer))
 			 $SellerNick = "seller".$rpw["id"]."";
      echo "
- <tr
-    <td id='id'> ".htmlspeciapchars($row['id'])."</td>
-    <td id='country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;".htmlspecialchars($row['country'])." </td>
-    <td id='website'> ".htmlspecialchars($row['sitename'])." </td> 
-	<td id='details> ".htmlspecialchars($row['infos'])." </td>
-    <td id='idseller'> ".htmlspecialchars($SellerNick)."</td>
-    <td id='price'> ".htmlspecialchars($row['price'])."</td>
-    <td id='date'> ".htmlspecialchars($row['date'])."</td>";
+ <tr>     
+    <td id='account_country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;".htmlspecialchars($row['country'])." </td>
+    <td id='account_sitename'> ".htmlspecialchars($row['sitename'])." </td> 
+	<td> ".htmlspecialchars($row['infos'])." </td>
+    <td id='account_seller'> ".htmlspecialchars($SellerNick)."</td>
+    <td> ".htmlspecialchars($row['price'])."</td>
+	    <td> ".$row['date']."</td>";
     echo '
     <td>
 	<span id="premium'.$row['id'].'" title="buy" type="premium"><a onclick="javascript:buythistool('.$row['id'].')" class="btn btn-primary btn-xs"><font color=white>Buy</font></a></span><center>
@@ -125,6 +125,7 @@ $q = mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0' ORDER BY RAND()
  }
 
  ?>
+
 </div>
 </div>
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
