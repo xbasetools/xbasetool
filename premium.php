a209c8887ae4072a3bd24d1eebd28ac034f7cecd
xbasetools <127048636+xbasetools@users.noreply.github.com>
2023-03-08 03:05:04 -0800

    Update premium.php

--- premium.php
+++ premium.php
@@ -497,8 +497,8 @@ a.closearb {
 </div>
 
       <div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
-        <div class="col-sm-12 table-responsive">
-<table width=“100%”  class=“table table-striped table-bordered table-condensed sticky-header” id=“table”>
+    <div class="col-sm-12 myTable-responsive">
+<table width=“100%”  class=“myTable table-striped table-bordered table-condensed sticky-header” id=“table”>
 <thead>
     <tr>
       <th scope=“col” >ID</th>
@@ -542,6 +542,7 @@ $q = mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0' ORDER BY RAND()
 
  ?>
 <script type="text/javascript">
+$(function(){$("#myTable").dataTable(); })
 $('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#account_country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#account_sitename").text().toLowerCase() );var ck3 = $.trim( $(this).find("#account_seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="account_country"]').val().toLowerCase() );var val2 = $.trim( $('input[name="account_sitename"]').val().toLowerCase() );var val3 = $.trim( $('select[name="account_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != '' )){ $(this).hide();  }else{ $(this).show(); } });$('#filterbutton').prop('disabled', true);});$('.filterselect').change(function () {$('#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
 function buythistool(id){
   bootbox.confirm("Are you sure?", function(result) {
