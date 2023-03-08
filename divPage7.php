9e891c000ac1e3a1264ba5de86a83303405005cc
xbasetools <127048636+xbasetools@users.noreply.github.com>
2023-03-08 09:37:32 -0800

    Update divPage7.php

--- divPage7.php
+++ divPage7.php
@@ -1,15 +1,4 @@
-hi<?php
-ob_start();
-session_start();
-date_default_timezone_set('UTC');
-include "includes/config.php";
 
-if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
-    header("location: ../");
-    exit();
-}
-$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-?>
 
 <?php
 ob_start();
@@ -24,12 +13,11 @@ if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>
 
-<ul class="nav nav-tabs">
-  <li class="active"><a href="#filter" data-toggle="tab">Filter</a></li>
-</ul>
+<ul class="nav nav-tabs"> <li class="active"><a href="#filter" data-toggle="tab">Filter</a></li></ul>
+
 <div id="myTabContent" class="tab-content" >
-  <div class="tab-pane active in" id="filter"><table class="table"><thead><tr><th>Country</th>
-<th>Site Name</th>
+ <div class="tab-pane active in" id="filter"><table class="table"><thead><tr><th>Country</th>
+<th>Website Name</th>
 <th>Seller</th>
 <th></th></tr></thead><tbody><tr><td><select class='filterselect form-control input-sm' name="account_country"><option value="">ALL</option>
 <?php
@@ -45,9 +33,7 @@ $query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `accounts` WHER
 		 $qer = mysqli_query($dbcon, "SELECT DISTINCT(`id`) FROM resseller WHERE username='".$row['resseller']."' ORDER BY id ASC")or die(mysql_error());
 		   while($rpw = mysqli_fetch_assoc($qer))
 			 $SellerNick = "seller".$rpw["id"]."";
-	echo '<option value="'.$SellerNick.'">'.$SellerNick.'</option>';
-	}
-?>
+	echo '<option value="'.$SellerNick.'">'.$SellerNick.'</option>';}?>
 </select></td><td><button id='filterbutton'class="btn btn-primary btn-sm" disabled>Filter <span class="glyphicon glyphicon-filter"></span></button></td></tr></tbody></table></div>
 </div>
 
