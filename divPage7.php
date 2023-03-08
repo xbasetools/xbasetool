e683cb6c15e4378eb89a5abaa696f2600f6a47a1
xbasetools <127048636+xbasetools@users.noreply.github.com>
2023-03-08 12:27:45 -0800

    Update divPage7.php

--- divPage7.php
+++ divPage7.php
@@ -117,8 +117,34 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
               </tr>
             </thead>
           </table>
-        
-        
+         <?php
+include(“cr.php”);
+$q = mysqli_query($dbcon, “SELECT * FROM accounts WHERE sold=‘0’ ORDER BY RAND()”)or die(mysqli_error());
+ while($row = mysqli_fetch_assoc($q)){
+	 
+	 	 $countryfullname = $row[‘country’];
+	  $code = array_search(“$countryfullname”, $countrycodes);
+	 $countrycode = strtolower($code);
+	    $qer = mysqli_query($dbcon, “SELECT * FROM resseller WHERE username=‘”.$row[‘resseller’].”’”)or die(mysql_error());
+		   while($rpw = mysqli_fetch_assoc($qer))
+			 $SellerNick = “seller”.$rpw[“id”].””;
+     echo “
+ <tr>     
+    <td id=‘account_country’><i class=‘flag-icon flag-icon-$countrycode’></i>&nbsp;”.htmlspecialchars($row[‘country’]).” </td>
+    <td id=‘account_sitename’> “.htmlspecialchars($row[‘sitename’]).” </td> 
+	<td> “.htmlspecialchars($row[‘infos’]).” </td>
+    <td id=‘account_seller’> “.htmlspecialchars($SellerNick).”</td>
+    <td> “.htmlspecialchars($row[‘price’]).”</td>
+	    <td> “.$row[‘date’].”</td>”;
+    echo ‘
+    <td>
+	<span id=“premium’.$row[‘id’].’” title=“buy” type=“premium”><a onclick=“javascript:buythistool(‘.$row[‘id’].’)” class=“btn btn-primary btn-xs”><font color=white>Buy</font></a></span><center>
+    </td>
+            </tr>
+     ‘;
+ }
+
+ ?>
         </div>
       </div>
       <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
