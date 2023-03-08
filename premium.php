8d469d2fec61d7920d7a432e6ef1aea55b3d6297
xbasetools <127048636+xbasetools@users.noreply.github.com>
2023-03-08 10:27:59 -0800

    Update premium.php

--- premium.php
+++ premium.php
@@ -449,28 +449,12 @@ $q = mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0' ORDER BY RAND()
 
  ?>
 <script type="text/javascript">
-    $(document).ready(function() {
-      $('#table').DataTable({
-        "fnCreatedRow": function(nRow, aData, iDataIndex) {
-          $(nRow).attr('id', aData[0]);
-        },
-        'serverSide': 'true',
-        'processing': 'true',
-        'paging': 'true',
-        'order': [],
-        'ajax': {
-          'url': 'divPage7.html',
-          'type': 'post',
-        },
-        "aoColumnDefs": [{
-            "bSortable": false,
-            "aTargets": [5]
-          },
-
-        ]
-      });
-    });
-$('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#account_country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#account_sitename").text().toLowerCase() );var ck3 = $.trim( $(this).find("#account_seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="account_country"]').val().toLowerCase() );var val2 = $.trim( $('input[name="account_sitename"]').val().toLowerCase() );var val3 = $.trim( $('select[name="account_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != '' )){ $(this).hide();  }else{ $(this).show(); } });
+$('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#account_country").text().toLowerCase() );
+  var ck2 = $.trim( $(this).find("#account_sitename").text().toLowerCase() );
+  var ck3 = $.trim( $(this).find("#account_seller").text().toLowerCase() ); 
+  var val1 = $.trim( $('select[name="account_country"]').val().toLowerCase() );
+  var val2 = $.trim( $('input[name="account_sitename"]').val().toLowerCase() );
+  var val3 = $.trim( $('select[name="account_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != '' )){ $(this).hide();  }else{ $(this).show(); } });
 $('#filterbutton').prop('disabled', true);});$('.filterselect').change(
   function () {$'#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
 
