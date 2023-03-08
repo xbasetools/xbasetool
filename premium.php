d7cd20ba4833767b52a92bba0897333badb3c0ba
xbasetools <127048636+xbasetools@users.noreply.github.com>
2023-03-08 09:28:10 -0800

    Update premium.php

--- premium.php
+++ premium.php
@@ -100,7 +100,21 @@
             </div>
           </div>
         </div>
-        <script>
+
+  <!-- Optional JavaScript; choose one of the two! -->
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="code.jquery.com/jquery-3.6.3.min.js" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.bundle.min.js" integrity="sha512-i9cEfJwUwViEPFKdC1enz4ZRGBj8YQo6QByFTF92YXHi7waCqyexvRD75S5NVTsSiTv7rKWqG9Y5eFxmRsOn0A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script type="text/javascript" src="js/jquery.datatables.min.js"></script>
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <script src="js/premium.js">
+   <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+  -->
+
+
+              <script>
       $(document).ready( function () {
     $('#table').DataTable();} );
           
@@ -139,9 +153,6 @@
         $("#modelbody").append(data);
         $('#myModal').modal();
         }});  }
-        </script>
-        <script src="js/premium.js">
-        
         </script>
       </body>
     </html>
