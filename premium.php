31eadcefd3e0d6dcd1d71ed1c97818fb3bd00a5f
xbasetools <127048636+xbasetools@users.noreply.github.com>
2023-03-08 02:19:26 -0800

    Update premium.php

--- premium.php
+++ premium.php
@@ -582,15 +582,20 @@ a.closearb {
 
 </div>
 
-<div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
-<div class="col-sm-12 table-responsive">
-<table id="account_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
-<thead>
 <tr>
-<th data-priority="1"></th>
- 
- 
- 
+<table width=“100%”  class=“table table-striped table-bordered table-condensed sticky-header” id=“account_data”>
+<thead>
+    <tr>
+      <th scope=“col” >Country</th>
+      <th scope=“col”>Site Name</th>
+      <th scope=“col”>Website Name</th>
+      <th scope=“col”>Seller</th>
+      <th scope=“col”>Price</th>
+      <th scope=“col”>Added on </th>
+      <th scope=“col”>Buy</th>
+    </tr>
+</thead>
+  <tbody>
 </div>
 </div>
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
