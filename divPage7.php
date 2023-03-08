6b77c447ccfc562603d9e111ab32ab5e6ffb4529
xbasetools <127048636+xbasetools@users.noreply.github.com>
2023-03-08 07:19:42 -0800

    Update divPage7.php

--- divPage7.php
+++ divPage7.php
@@ -78,28 +78,21 @@ $query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `accounts` WHER
 </div>
 
  
- 
 
- 
- 
-<table width="100%" class="table table-striped table-bordered table-condensed sticky-header" id="table">
-    <thead>
-        <tr>
-            <!---<th scope="col">id </th>-->
-            <th scope="col">Country</th>
-            <th scope="col">Website</th>
-            <th scope="col">Details</th>
-            <th scope="col">Seller</th>
-            <th scope="col">Price</th>
-            <th scope="col">Added Date </th>
-            <th scope="col">Buy</th>
-        </tr>
-    </thead>
-    <tbody>
- 
-    </tbody>
-  </table>
-</div>
+
+<table width="100%"  class="table table-striped table-bordered table-condensed sticky-header" id="table">
+<thead>
+    <tr>
+      <th scope="col" >Country</th>
+      <th scope="col">Site Name</th>
+      <th scope="col">Available Information</th>
+      <th scope="col">Seller</th>
+      <th scope="col">Price</th>
+      <th scope="col">Added on </th>
+      <th scope="col">Buy</th>
+    </tr>
+</thead>
+  <tbody>
 
  <?php
 include("cr.php");
@@ -129,8 +122,6 @@ $q = mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0' ORDER BY RAND()
  }
 
  ?>
-
-</div>
 </div>
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
